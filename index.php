a2e2a0608386d6fa9225cdc95258a6f222444b91
AnnGizmo <omggizmo19@mail.ru>
2024-12-15 16:11:29 +0300

    правки

--- index.php
+++ index.php
@@ -132,12 +132,12 @@ $lots = [
                     <img src="" width="350" height="260" alt="">
                 </div>
                 <div class="lot__info">
-                    <span class="lot__category"><?= $lot['categories'] ?></span>
+                    <span class="lot__category"><?= $lot["category"] ?></span>
                     <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><? $lots['name'] ?></a></h3>
                     <div class="lot__state">
                         <div class="lot__rate">
                             <span class="lot__amount">Начальная цена</span>
-                            <span class="lot__cost">?= formatPrice($lot['price']) ?></span>
+                            <span class="lot__cost"><?= formatPrice($lot["price"]) ?></span>
                         </div>
                         <div class="lot__timer timer">
                             12:23
